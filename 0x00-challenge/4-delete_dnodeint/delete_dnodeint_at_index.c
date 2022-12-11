ec8abab742005e2bce935b764622b6004d98f36a
EndrisMoh <endrismd@gmail.com>
2022-12-11 14:04:44 -0800

    Double linked list

--- 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
+++ 0x00-challenge/4-delete_dnodeint/delete_dnodeint_at_index.c
@@ -11,50 +11,50 @@
  */
 int delete_dnodeint_at_index(dlistint_t **head, unsigned int index)
 {
-	dlistint_t *saved_head;
-	dlistint_t *tmp;
-	unsigned int p;
+		dlistint_t *saved_head;
+		dlistint_t *tmp;
+		unsigned int p;
 
-	if (*head == NULL)
-	{
-		return (-1);
-	}
-
-	saved_head = *head;
-	p = 0;
-
-	while (p < index && *head != NULL)
-	{
-		*head = (*head)->next;
-		p++;
-	}
-
-	if (p != index)
-	{
-		*head = saved_head;
-		return (-1);
-	}
-
-	if (0 == index)
-	{
-		tmp = (*head)->next;
-		free(*head);
-		*head = tmp;
-		if (tmp != NULL)
+		if (*head == NULL)
 		{
-			tmp->prev = NULL;
+				return (-1);
 		}
-	}
-	else
-	{
-		(*head)->prev->prev = (*head)->prev;
-		free(*head);
 
-		if ((*head)->next)
-			(*head)->next->prev = (*head)->prev;
+		saved_head = *head;
+		p = 0;
 
-		free(*head);
-		*head = saved_head;
-	}
-	return (1);
+		while (p < index && *head != NULL)
+		{
+				*head = (*head)->next;
+				p++;
+		}
+
+		if (p != index)
+		{
+				*head = saved_head;
+				return (-1);
+		}
+
+		if (0 == index)
+		{
+				tmp = (*head)->next;
+				free(*head);
+				*head = tmp;
+				if (tmp != NULL)
+				{
+						tmp->prev = NULL;
+				}
+		}
+		else
+		{
+				if ((*head)->prev)
+						(*head)->prev->next = (*head)->next;
+
+				if ((*head)->next)
+						(*head)->next->prev = (*head)->prev;
+
+				free(*head);
+				*head = saved_head;
+		}
+		return (1);
 }
